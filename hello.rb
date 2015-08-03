cbea9f9abe22882d7b29621c69413040856ba388
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-08-03 12:47:15 +0100

    added dotted red

--- hello.rb
+++ hello.rb
@@ -1,13 +1,7 @@
 require 'sinatra'
 
-get '/' do 
-  "<div>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
+get '/' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
-
-get '/secret' do 
-   "<div>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
-end  
